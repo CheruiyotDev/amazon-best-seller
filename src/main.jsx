76d879779e547163de2dfb43d48bd1aba5caca6b
Evans <chersoftwaredev@gmail.com>
2024-12-15 08:30:55 +0300

    Introduction to components instances

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+
+const BookList = () => {
+  return (
+    <section>
+      <Book />
+    </section>
+  );
+};
+
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => <h2>image placeholder</h2>;
+const Title = () => <h2>Book Title</h2>;
+const Author = () => {
+  return <h4>Author</h4>;
+};
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BookList />
+  </StrictMode>
+);
