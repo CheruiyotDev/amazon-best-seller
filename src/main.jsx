db8ca09327ed25a6253c91ed6ef7e2ca23e21836
Evans <chersoftwaredev@gmail.com>
2025-01-22 14:27:09 +0300

    prop drilling

--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,82 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
+
+const books = [
+  {
+    author: 'Jordan Moore',
+    title: 'Interesting Facts For Curious Minds',
+    img: './images/book-1.jpg',
+    id: 1,
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic  Habits',
+    img: './images/book-2.jpg',
+    id: 2,
+  },
+  {
+    author: 'Elon Musk',
+    title: 'Software Development',
+    img: './images/book-3.jpg',
+    id: 3,
+  },
+  {
+    author: 'Mark Zuckerbark',
+    title: 'Introduction to React',
+    img: './images/book-4.jpg',
+    id: 4,
+  },
+  {
+    author: 'John Smilga',
+    title: 'Fullstack Web Development',
+    img: './images/book-5.jpg',
+    id: 5,
+  },
+];
 
 const BookList = () => {
   return (
-    <section>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+    <section className='booklist'>
+      <EventExamples />
+      {books.map((book) => {
+        console.log(book);
+        return <Book book={book} key={book.id} />;
+      })}
     </section>
   );
 };
 
-const Book = () => {
+const EventExamples = () => {
   return (
-    <article>
-      <Image />
-      <Title />
-      <Author />
-    </article>
+    <section>
+      <form>
+        <h2>Typical Form</h2>
+        <input
+          type='text'
+          name='example'
+          onChange={(e) => console.log(e.target.value)}
+          style={{ margin: '1rem 0' }}
+        />
+        <button type='submit'>submit form</button>
+        <div>
+          <button>click me</button>
+        </div>
+      </form>
+    </section>
   );
 };
 
-const Image = () => (
-  <img
-    src='https://images-na.ssl-images-amazon.com/images/I/71sOqrd6JHL._AC_UL600_SR600,400_.jpg'
-    alt=''
-  />
-);
-const Title = () => <h2>Mom, I Want to Hear Your Story:</h2>;
-const Author = () => {
-  return <h4> Jeffrey Mason</h4>;
+const Book = (props) => {
+  const { img, title, author } = props.book;
+  console.log(props.book);
+  return (
+    <article className='book'>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+    </article>
+  );
 };
 
 createRoot(document.getElementById('root')).render(
