99e831dafe9955bb1295dc25fdc33964ed5104b5
Evans <chersoftwaredev@gmail.com>
2025-01-22 20:33:13 +0300

    amazon best sellers"

---
+++ src/books.jsx
@@ -0,0 +1,38 @@
+import img1 from './images/book-1.jpg';
+import img2 from './images/book-2.jpg';
+import img3 from './images/book-3.jpg';
+import img4 from './images/book-4.jpg';
+import img5 from './images/book-5.jpg';
+
+export const books = [
+  {
+    author: 'Jordan Moore',
+    title: 'Interesting Facts For Curious Minds',
+    img: img1,
+    id: 1,
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic  Habits',
+    img: img2,
+    id: 2,
+  },
+  {
+    author: 'Elon Musk',
+    title: 'Software Development',
+    img: img3,
+    id: 3,
+  },
+  {
+    author: 'Mark Zuckerbark',
+    title: 'Introduction to React',
+    img: img4,
+    id: 4,
+  },
+  {
+    author: 'John Smilga',
+    title: 'Fullstack Web Development',
+    img: img5,
+    id: 5,
+  },
+];
--- src/main.jsx
+++ src/main.jsx
@@ -1,81 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-
-const books = [
-  {
-    author: 'Jordan Moore',
-    title: 'Interesting Facts For Curious Minds',
-    img: './images/book-1.jpg',
-    id: 1,
-  },
-  {
-    author: 'James Clear',
-    title: 'Atomic  Habits',
-    img: './images/book-2.jpg',
-    id: 2,
-  },
-  {
-    author: 'Elon Musk',
-    title: 'Software Development',
-    img: './images/book-3.jpg',
-    id: 3,
-  },
-  {
-    author: 'Mark Zuckerbark',
-    title: 'Introduction to React',
-    img: './images/book-4.jpg',
-    id: 4,
-  },
-  {
-    author: 'John Smilga',
-    title: 'Fullstack Web Development',
-    img: './images/book-5.jpg',
-    id: 5,
-  },
-];
+import { books } from './books';
+import Book from './Book';
 
 const BookList = () => {
   return (
-    <section className='booklist'>
-      <EventExamples />
-      {books.map((book) => {
-        console.log(book);
-        return <Book book={book} key={book.id} />;
-      })}
-    </section>
-  );
-};
-
-const EventExamples = () => {
-  return (
-    <section>
-      <form>
-        <h2>Typical Form</h2>
-        <input
-          type='text'
-          name='example'
-          onChange={(e) => console.log(e.target.value)}
-          style={{ margin: '1rem 0' }}
-        />
-        <button type='submit'>submit form</button>
-        <div>
-          <button>click me</button>
-        </div>
-      </form>
-    </section>
-  );
-};
-
-const Book = (props) => {
-  const { img, title, author } = props.book;
-  console.log(props.book);
-  return (
-    <article className='book'>
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-    </article>
+    <>
+      <h1>amazon best sellers</h1>
+      <section className='booklist'>
+        {books.map((book, index) => {
+          console.log(book);
+          return <Book {...book} key={book.id} number={index} />;
+        })}
+      </section>
+    </>
   );
 };
 
